59975703362ee392f6925d6e91e05a2a05a1a930
Mateo Elías <oscarmateoelias@gmail.com>
2022-12-29 19:03:35 -0600

    Create - mensaje de error dinamico

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const form = document.querySelector('#formulario');
 
     // Asignar eventos
     inputEmail.addEventListener('blur', validar);
@@ -12,11 +13,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function validar(e) {
         const input = e.target.value;
+        const type = e.target.id;
+        const divReference = e.target.parentElement;
 
         if (!(input.trim())) {
-            console.log('vacio');
+            mostrarAlerta(type, divReference);
         } else {
-            console.log('Algo');
+
+        }
+
+        function mostrarAlerta(type, reference) {
+            // Condicional si ya existe una alerta o no
+            const alerta = reference.querySelector('.alerta');
+
+            if (alerta) {
+                alerta.remove();
+            }
+
+            // Generar alerta en HTML
+            const error = document.createElement('P');
+            error.textContent = `El campo ${type} esta vacío`;
+            error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'alerta');
+
+            // Inyectar el error al formulario
+            reference.appendChild(error);
+
+            console.log(error);
         }
     }
 });
