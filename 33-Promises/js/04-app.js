d09a737810c66794585a362a0d5fc983b9d8de1c
Mateo Elías <oscarmateoelias@gmail.com>
2023-06-07 17:41:31 -0600

    Callback Hell solucionado -Promises

---
+++ 33-Promises/js/04-app.js
@@ -0,0 +1,23 @@
+const paises = [];
+
+const nuevoPais = pais => new Promise(resolve => {
+    setTimeout(() => {
+        paises.push(pais);
+        resolve(`Agregado: ${pais}`)
+    }, 3000);
+})
+
+nuevoPais('Alemania')
+    .then(resultado => {
+        console.log(resultado);
+        console.log(paises);
+        return nuevoPais('Francia')
+    })
+    .then(resultado => {
+        console.log(resultado);
+        return nuevoPais('El Salvador')
+    })
+    .then(resultado => {
+        console.log(resultado);
+        console.log(paises);
+    })
