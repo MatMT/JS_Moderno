7dbf9cebf21c6cdd9b73686f707b0f6bdc1297de
Mateo Elías <oscarmateoelias@gmail.com>
2023-07-07 11:24:02 -0600

    Pixabay a Async Await

--- 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ 42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -14,10 +14,10 @@ window.onload = () => {
 
 function validarFormulario(e) {
     e.preventDefault();
-    
+
     const terminoBusqueda = document.querySelector('#termino').value;
-    
-    if(terminoBusqueda === '' ) {
+
+    if (terminoBusqueda === '') {
         mostrarAlerta('Agrega un término de búsqueda');
         return;
     }
@@ -29,61 +29,70 @@ function mostrarAlerta(mensaje) {
 
     const existeAlerta = document.querySelector('.bg-red-100');
 
-    if(!existeAlerta) {
+    if (!existeAlerta) {
         const alerta = document.createElement('p');
         alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
-    
+
         alerta.innerHTML = `
             <strong class="font-bold">Error!</strong>
             <span class="block sm:inline">${mensaje}</span>
         `;
-    
+
         formulario.appendChild(alerta);
-    
+
         setTimeout(() => {
             alerta.remove();
         }, 3000);
     }
-    
+
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '37725079-e012b407f6477637f1e78e79e';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
+    // fetch(url)
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         mostrarImagenes(resultado.hits);
+    //     });
+
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
-function *crearPaginador(total) {
-    for (let i = 1; i <= total; i++ ) {
+function* crearPaginador(total) {
+    for (let i = 1; i <= total; i++) {
         yield i;
     }
 }
 
 
 function calcularPaginas(total) {
-    return parseInt( Math.ceil( total / registrosPorPagina ));
+    return parseInt(Math.ceil(total / registrosPorPagina));
 }
 
 function mostrarImagenes(imagenes) {
     // console.log(imagenes);
-    
-    while(resultado.firstChild) {
+
+    while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
 
     // Iterar sobre el arreglo de imagenes y construir el HTML
-    imagenes.forEach( imagen => {
+    imagenes.forEach(imagen => {
         const { previewURL, likes, views, largeImageURL } = imagen;
 
         resultado.innerHTML += `
@@ -109,7 +118,7 @@ function mostrarImagenes(imagenes) {
     });
 
     // Limpiar el paginador previo
-    while(paginacionDiv.firstChild) {
+    while (paginacionDiv.firstChild) {
         paginacionDiv.removeChild(paginacionDiv.firstChild)
     }
 
@@ -122,9 +131,9 @@ function mostrarImagenes(imagenes) {
 function imprimirPaginador() {
     iterador = crearPaginador(totalPaginas);
 
-    while(true) {
-        const { value, done} = iterador.next();
-        if(done) return;
+    while (true) {
+        const { value, done } = iterador.next();
+        if (done) return;
 
         // Caso contrario, genera un botón por cada elemento en el generador
         const boton = document.createElement('a');
