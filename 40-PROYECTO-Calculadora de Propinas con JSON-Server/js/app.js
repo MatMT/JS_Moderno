f401cdf19f85fe730bfbe1f6e5e2b5c3d300ef25
Mateo Elías <oscarmateoelias@gmail.com>
2023-07-06 18:03:36 -0600

    Listado de Pedido CRUD

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -67,7 +67,7 @@ function mostrarSecciones() {
 }
 
 function obtenerPlatillos() {
-    const url = "http://localhost:3000/db";
+    const url = "http://localhost:4000/db";
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -101,6 +101,13 @@ function mostrarPlatillos({ platillos }) {
         inputCantidad.id = `producto-${platillo.id}`;
         inputCantidad.classList.add('form-control');
 
+        // Función que detecta la cantidad y el platillo que se esta agregando
+        inputCantidad.onchange = function () {
+            const cantidad = inputCantidad.value;
+
+            agregarPlatillo({ ...platillo, cantidad });
+        }
+
         const agregar = document.createElement('DIV');
         agregar.classList.add('col-md-2');
         agregar.appendChild(inputCantidad);
@@ -111,4 +118,211 @@ function mostrarPlatillos({ platillos }) {
         row.appendChild(agregar);
         contenido.appendChild(row);
     })
+}
+
+function agregarPlatillo(producto) {
+    // Extraer el pedido actual en el objeto Global del cliente
+    let { pedido } = cliente;
+
+    // Revisar que la cantidad sea mayor a 0
+    if (producto.cantidad > 0) {
+        // Some, retorna un booleano
+        let existe = pedido.some(articulo => articulo.id === producto.id);
+
+        // Comprueba si el elemento ya existe en el array
+        if (existe) {
+            // Actualizar cantidad modificada, map retorna un nuevo arreglo
+            const pedidoActualizado = pedido.map(articulo => {
+                if (articulo.id === producto.id) {
+                    articulo.cantidad = producto.cantidad;
+                }
+
+                // Se retorna la variable temporal modificada
+                return articulo;
+            });
+
+            // Se asigna el nuevo array a cliente.pedido
+            cliente.pedido = [...pedidoActualizado];
+        } else {
+            // No existe, es agregado al arreglo
+            cliente.pedido = [...pedido, producto];
+        }
+    } else {
+        // Eliminar elementos cuando la cantidad es 0
+        const resultado = pedido.filter(articulo => articulo.id !== producto.id);
+        cliente.pedido = [...resultado];
+    }
+
+    // Limpiar el código HTMl previo
+    limpiarHTML();
+
+    // Mostrar el Resumen
+    if (cliente.pedido.length) {
+        actualizarResumen();
+    } else {
+        mensajePedidoVacio()
+    }
+
+}
+
+// Mostrar el Resumen
+function actualizarResumen() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    // Scripting
+    const resumen = document.createElement('DIV');
+    resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
+
+    // Información de la mesa
+    const mesa = document.createElement('P');
+    mesa.textContent = 'Mesa: ';
+    mesa.classList.add('fw-bold');
+
+    const mesaSpan = document.createElement('SPAN');
+    mesaSpan.textContent = cliente.mesa;
+    mesaSpan.classList.add('fw-normal');
+
+    // Información de la hora
+    const hora = document.createElement('P');
+    hora.textContent = 'Hora: ';
+    hora.classList.add('fw-bold');
+
+    const horaSpan = document.createElement('SPAN');
+    horaSpan.textContent = cliente.hora;
+    horaSpan.classList.add('fw-normal');
+
+    // Agregar a los elementos padre
+    mesa.appendChild(mesaSpan);
+    hora.appendChild(horaSpan);
+
+    // Titulo de la sección
+    const heading = document.createElement('H3');
+    heading.textContent = 'Platillos Consumidos';
+    heading.classList.add('my-4', 'text-center');
+
+    // Iterar sobre el array de pedidos
+    const grupo = document.createElement('UL');
+    grupo.classList.add('list-group');
+
+    const { pedido } = cliente;
+    pedido.forEach(articulo => {
+        const { nombre, cantidad, precio, id } = articulo;
+
+        const lista = document.createElement('LI');
+        lista.classList.add('list-group-item');
+
+        const nombreEl = document.createElement('H4');
+        nombreEl.classList.add('my-4');
+        nombreEl.textContent = nombre;
+
+        // Cantidad del articulo
+        const cantidadEl = document.createElement('P');
+        cantidadEl.classList.add('fw-bold');
+        cantidadEl.textContent = 'Cantidad: ';
+
+        const cantidadValor = document.createElement('SPAN');
+        cantidadValor.classList.add('fw-normal');
+        cantidadValor.textContent = cantidad;
+
+        // Precio del articulo
+        const precioEl = document.createElement('P');
+        precioEl.classList.add('fw-bold');
+        precioEl.textContent = 'Precio: ';
+
+        const precioValor = document.createElement('SPAN');
+        precioValor.classList.add('fw-normal');
+        precioValor.textContent = `$${precio}`;
+
+        // Subtotal del articulo
+        const subtotalEl = document.createElement('P');
+        subtotalEl.classList.add('fw-bold');
+        subtotalEl.textContent = 'Subtotal: ';
+
+        const subtotalValor = document.createElement('SPAN');
+        subtotalValor.classList.add('fw-normal');
+        subtotalValor.textContent = calcularSubtotal(precio, cantidad);
+
+        // Btn Eliminar
+        const btnEliminar = document.createElement('BUTTON');
+        btnEliminar.classList.add('btn', 'btn-danger');
+        btnEliminar.textContent = 'Eliminar del Pedido';
+
+        // Función para eliminar del pedido
+        btnEliminar.onclick = () => eliminarProducto(id);
+
+        // Agregar valores a sus contenedores
+        cantidadEl.appendChild(cantidadValor);
+        precioEl.appendChild(precioValor);
+        subtotalEl.appendChild(subtotalValor);
+
+        // Agregar elementos al LI
+        lista.appendChild(nombreEl);
+        lista.appendChild(cantidadEl);
+        lista.appendChild(precioEl);
+        lista.appendChild(subtotalEl);
+        lista.appendChild(btnEliminar);
+
+        // Agregar lista al grupo principal
+        grupo.appendChild(lista);
+
+        // Mostrar formulario de propinas
+        formularioPropinas();
+    })
+
+    // Agregar al contenido
+    resumen.appendChild(mesa);
+    resumen.appendChild(hora);
+    resumen.appendChild(heading);
+    resumen.appendChild(grupo);
+
+
+    contenido.appendChild(resumen)
+}
+
+function limpiarHTML() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    // Mientras hayan elementos en el contenido
+    while (contenido.firstChild) {
+        contenido.removeChild(contenido.firstChild);
+    }
+}
+
+function calcularSubtotal(precio, cantidad) {
+    return `$${(precio * cantidad)}`;
+}
+
+function eliminarProducto(id) {
+    const { pedido } = cliente;
+
+    // Eliminar elementos cuando la cantidad es 0
+    const resultado = pedido.filter(articulo => articulo.id !== id);
+    cliente.pedido = [...resultado];
+
+    limpiarHTML();
+
+    // Mostrar el Resumen
+    if (cliente.pedido.length) {
+        actualizarResumen();
+    } else {
+        mensajePedidoVacio()
+    }
+
+    // Buscamos mediante el id del input, regresamos la cantidad a 0
+    const productoInput = document.querySelector(`#producto-${id}`);
+    productoInput.value = 0;
+}
+
+function mensajePedidoVacio() {
+    const contenido = document.querySelector('#resumen .contenido');
+
+    const texto = document.createElement('P');
+    texto.classList.add('text-center');
+    texto.textContent = 'Añade los elementos del pedido';
+
+    contenido.appendChild(texto);
+}
+
+function formularioPropinas() {
+
 }
